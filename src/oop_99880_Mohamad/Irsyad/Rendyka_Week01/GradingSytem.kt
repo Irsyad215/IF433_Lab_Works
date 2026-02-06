a0361467c1ec05989c892a6a4fc67bad342de597
Cmardo215 <Mohammadirsyadrendyka@gmail.com>
2026-02-06 20:24:30 +0700

    Week01: Initial setup and procedural code

---
+++ src/oop_99880_Mohamad/Irsyad/Rendyka_Week01/GradingSytem.kt
@@ -0,0 +1,8 @@
+package oop_99880_Mohamad.Irsyad.Rendyka_Week01
+
+fun main () {
+    var name: String =  "john thor"
+    var score: Int = 80
+
+    println("nama: " + name + ", Nilai: " + score)
+}
