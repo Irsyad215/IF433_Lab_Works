ec1b8b590cfcd4a4a939117104ca0f5269bff65a
Cmardo215 <Mohammadirsyadrendyka@gmail.com>
2026-02-07 00:04:39 +0700

    week01: (tugas 2) implement null safety for user note

--- src/oop_99880_Mohamad/Irsyad/Rendyka_Week01/GameStore.kt
+++ src/oop_99880_Mohamad/Irsyad/Rendyka_Week01/GameStore.kt
@@ -1,18 +1,21 @@
 package oop_99880_Mohamad.Irsyad.Rendyka_Week01
 
-fun calculateDiscount(price: Int): Int = if (price > 500000) (price * 0.2).toInt() else (price * 0.1).toInt()
-
 fun main() {
     val gameTittle = "Elden Ring"
     val price = 600000
+    val usernote: String?= null
+
     val discount = calculateDiscount (price)
     val finalPrice = price - discount
 
-    printReceipt(tittle = gameTittle, finalPrice = finalPrice)
+    printReceipt(tittle = gameTittle, finalPrice = finalPrice, note = usernote)
 }
 
-fun printReceipt(tittle: String, finalPrice: Int) {
+fun calculateDiscount(price: Int): Int = if (price > 500000) (price * 0.2).toInt() else (price * 0.1).toInt()
+
+fun printReceipt(tittle: String, finalPrice: Int, note: String?) {
     println("--- Struk Pembelian ---")
     println("Game: $tittle")
     println("Harga Akhir: Rp $finalPrice")
+    println("catatan: ${note ?: "Game Souls-Like open world dari FromSoftware"}")
 }
