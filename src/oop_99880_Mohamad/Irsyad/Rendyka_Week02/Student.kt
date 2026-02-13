847eabde7d79af0a1cc388beacfaafff73eb7dcf
Cmardo215 <Mohammadirsyadrendyka@gmail.com>
2026-02-13 10:26:37 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_99880_Mohamad/Irsyad/Rendyka_Week02/Student.kt
+++ src/oop_99880_Mohamad/Irsyad/Rendyka_Week02/Student.kt
@@ -1,2 +1,16 @@
 package oop_99880_Mohamad.Irsyad.Rendyka_Week02
 
+class Student (
+    val name: String ,
+    val nim: String,
+    var major: String
+){
+    init {
+        if (nim.length != 5) {
+            println("WARNING, objek tercipta dengan NIM ($nim) tang tidak valid")
+            println("data mahasiswa $name mungkin akan bermasalah di sistem")
+        } else {
+            println("LOG: Objek student $name berhasil berkolaborasi di Memory")
+        }
+    }
+}
