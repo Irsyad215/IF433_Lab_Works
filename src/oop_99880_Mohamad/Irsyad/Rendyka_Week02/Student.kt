998afc7e55627da09d4dcf0c5b4b98f7c27e50b9
Cmardo215 <Mohammadirsyadrendyka@gmail.com>
2026-02-13 10:35:17 +0700

    week02: create basic Student class structure

--- src/oop_99880_Mohamad/Irsyad/Rendyka_Week02/Student.kt
+++ src/oop_99880_Mohamad/Irsyad/Rendyka_Week02/Student.kt
@@ -5,12 +5,5 @@ class Student (
     val nim: String,
     var major: String
 ){
-    init {
-        if (nim.length != 5) {
-            println("WARNING, objek tercipta dengan NIM ($nim) tang tidak valid")
-            println("data mahasiswa $name mungkin akan bermasalah di sistem")
-        } else {
-            println("LOG: Objek Student $name berhasil berkolaborasi di Memory")
-        }
-    }
+
 }
