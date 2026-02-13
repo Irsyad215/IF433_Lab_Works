895802aa2fda22d53686ca2d8bf44c63931b7c72
Cmardo215 <Mohammadirsyadrendyka@gmail.com>
2026-02-13 11:03:02 +0700

    Main.kt

--- src/oop_99880_Mohamad/Irsyad/Rendyka_Week02/Main.kt
+++ src/oop_99880_Mohamad/Irsyad/Rendyka_Week02/Main.kt
@@ -1,2 +1,28 @@
-package oop_99880_Mohamad.Irsyad.Rendyka_Week02
+package oop_99880_mohamad_irsyad.week02 // [cite: 8]
 
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`) //
+
+    println("--- APLIKASI PMB UMN ---")
+
+    print("Masukkan Nama: ")
+    val name = scanner.nextLine() //
+
+    print("Masukkan NIM (Wajib 5 Karakter): ")
+    val nim = scanner.next() //
+
+    scanner.nextLine() // Clear the buffer
+
+    if (nim.length != 5) {
+        println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!") //
+    } else {
+        print("Masukkan Jurusan: ")
+        val major = scanner.nextLine() // Fixed capital 'L'
+
+        // Instansiasi Objek
+        val s1 = Student(name, nim, major)
+        println("Status: Pendaftaran Selesai.") //
+    }
+}
--- src/oop_99880_Mohamad/Irsyad/Rendyka_Week02/Student.kt
+++ src/oop_99880_Mohamad/Irsyad/Rendyka_Week02/Student.kt
@@ -4,6 +4,19 @@ class Student (
     val name: String ,
     val nim: String,
     var major: String
-){
-
+) {
+    init {
+        if (nim.length != 5) {
+            println("WARNING, objek tercipta dengan NIM ($nim) tang tidak valid")
+            println("data mahasiswa $name mungkin akan bermasalah di sistem")
+        } else {
+            println("LOG: Objek student $name berhasil berkolaborasi di Memory")
+        }
+    }
+    constructor(name: String, nim: String) : this(name, nim, major = "Non-Matriculated") {
+        println("LOG: Menggunakan Constructor jalur umum (tanpa jurusan). ")
+    }
 }
+
+
+
