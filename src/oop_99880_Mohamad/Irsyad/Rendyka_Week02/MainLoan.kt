aea4f50ce2431ed366b5c40b97df41bd63010cf3
Cmardo215 <Mohammadirsyadrendyka@gmail.com>
2026-02-20 10:44:57 +0700

    week03: create basic Employee class

--- src/oop_99880_Mohamad/Irsyad/Rendyka_Week02/MainLoan.kt
+++ src/oop_99880_Mohamad/Irsyad/Rendyka_Week02/MainLoan.kt
@@ -1,4 +1,30 @@
-package oop_99880_Mohamad.Irsyad.Rendyka_Week02
+package oop_99880_mohamad_irsyad.week02
 
-class MainLoan {
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    println("=== SISTEM PERPUSTAKAAN UMN ===")
+
+    print("Judul Buku: ")
+    val title = scanner.nextLine()
+
+    print("Nama Peminjam: ")
+    val borrower = scanner.nextLine()
+
+    print("Lama Pinjam (Hari): ")
+    var duration = scanner.nextInt()
+
+    if (duration < 0) {
+        duration = 1
+    }
+
+    val loan1 = Loan(title, borrower, duration)
+
+    println("\n--- Detail Peminjaman ---")
+    println("Buku: ${loan1.bookTitle}")
+    println("Peminjam: ${loan1.borrower}")
+    println("Durasi: ${loan1.loanDuration} hari")
+    println("Total Denda: Rp ${loan1.calculateFine()}")
 }
---
+++ src/oop_99880_Mohamad/Irsyad/Week03/Employee.kt
@@ -0,0 +1,5 @@
+package oop_99880_Mohamad.Irsyad.Week03
+
+class Employee(val name: String) {
+    var salary: Int = 0
+}
