adb968721dcbebb916694a2a04555a52a636e688
Cmardo215 <Mohammadirsyadrendyka@gmail.com>
2026-02-20 10:59:46 +0700

    week03: fix setter using backing field

--- src/oop_99880_Mohamad/Irsyad/Week03/Employee.kt
+++ src/oop_99880_Mohamad/Irsyad/Week03/Employee.kt
@@ -2,4 +2,12 @@ package oop_99880_Mohamad.Irsyad.Week03
 
 class Employee(val name: String) {
     var salary: Int = 0
+        set(value) {
+            if (value < 0) {
+                println("ERROR: Gaji tidak boleh negatif DI-set ke 0")
+                field = 0
+            } else {
+                field = value
+            }
+        }
 }
--- src/oop_99880_Mohamad/Irsyad/Week03/Main.kt
+++ src/oop_99880_Mohamad/Irsyad/Week03/Main.kt
@@ -1,2 +1,7 @@
 package oop_99880_Mohamad.Irsyad.Week03
 
+fun main() {
+    val e = Employee("Budi")
+    println("--- Test Error ---")
+    e.salary = 5000
+}
