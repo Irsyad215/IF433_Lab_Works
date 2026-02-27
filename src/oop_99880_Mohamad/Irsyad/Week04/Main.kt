3885ea7dc2c834492deda9d6ab443a6a91cf3199
Cmardo215 <Mohammadirsyadrendyka@gmail.com>
2026-02-27 22:15:50 +0700

    week04: test Vehicle and Car in main

---
+++ src/oop_99880_Mohamad/Irsyad/Week04/Main.kt
@@ -0,0 +1,15 @@
+package oop_99880_Mohamad.Irsyad.Week04
+
+fun main() {
+    println("--- Testing Vehicle ---")
+    val generalVehicle = Vehicle(brand = "Sepeda Onthel")
+    generalVehicle.honk()
+    generalVehicle.accelerate()
+
+    println("\n--- Testing Car ---")
+    val myCar = Car(brand = "Toyota", numberOfDoors = 4)
+    myCar.openTrunk()
+    myCar.honk()
+    myCar.accelerate()
+}
+}
