0611857963d9faa9a75d49746e4df32fe658e2da
Cmardo215 <Mohammadirsyadrendyka@gmail.com>
2026-03-13 10:39:40 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_99880_Mohamad/Irsyad/Week06/GadgetInterfaces.kt
@@ -0,0 +1,9 @@
+package oop_99880_Mohamad.Irsyad.Week06
+
+interface Camera {
+    fun turnOn() {println("Lensa kamera terbuka dan sensor aktif") }
+}
+
+interface Phone () {
+    fun turnOn() {println("Sinyal seluler mencari jaringan") }
+}
