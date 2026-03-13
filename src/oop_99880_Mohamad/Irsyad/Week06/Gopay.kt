dafb5bc695f1417bd4d1aa9d62b0e82ba6de54c0
Cmardo215 <Mohammadirsyadrendyka@gmail.com>
2026-03-13 11:15:38 +0700

    error and adding PaymentImplementations also some typo's

--- src/oop_99880_Mohamad/Irsyad/Week06/Gopay.kt
+++
@@ -1,9 +0,0 @@
-package oop_99880_Mohamad.Irsyad.Week06
-
-class Gopay : PaymentMethod {
-    override fun pay(ammount: Double) { println("Processing Rp$ammount via gopay Server") }
-}
-
-class Credit : PaymentMethod {
-    override fun pay(ammount: Double) { println("Contacting bank for Rp$ammount") }
-}
