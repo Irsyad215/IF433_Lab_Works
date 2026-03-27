df048fde1ab0dc8e3dd38de38159fce0fd5fe3c4
Cmardo215 <Mohammadirsyadrendyka@gmail.com>
2026-03-27 11:03:47 +0700

    week07: resolve sealed class exhaustive check and fixing typos

--- src/oop_99880_Mohamad/Irsyad/Week07/Main.kt
+++ src/oop_99880_Mohamad/Irsyad/Week07/Main.kt
@@ -9,19 +9,19 @@ fun main() {
 
     println("\n=== TEST SINGLETON ===")
     val client = NetworkClient.createClient()
-    client.connect
+    client.connect()
 
     println("\n=== TEST REGULAR CLASS ===")
     val reg1 = RegularUser("Alice", 22)
     val reg2 = RegularUser("Alice", 22)
     println(reg1)
-    println("sama? ${reg1 === reg2}")
+    println("sama? ${reg1 == reg2}")
 
     println("\n=== TEST DATA CLASS ===")
     val data1 = Datauser("Alice", 22)
     val data2 = Datauser("Alice", 22)
     println(data1)
-    println("sama? ${data1 === data2}")
+    println("sama? ${data1 == data2}")
 
     val data3 = data1.copy(age = 23)
     println("Hasil Copy: $data3")
@@ -35,5 +35,6 @@ fun main() {
     val uiMessage = when (response) {
         is ApiResponse.Success -> "Tampilkan ${response.data}"
         is ApiResponse.Error -> "Munculkan alert: ${response.message}"
+        ApiResponse.Loading -> "Tampilkan Spinner"
     }
 }
--- src/oop_99880_Mohamad/Irsyad/Week07/NetworkClient.kt
+++ src/oop_99880_Mohamad/Irsyad/Week07/NetworkClient.kt
@@ -4,11 +4,12 @@ class NetworkClient private constructor(val url: String) {
 
     companion object {
         const val BASE_URL = "https://api.umn.ac.id"
-        println("Membangun NetworkClient dengan BASE_URL: $BASE_URL")
-        return NetworkClient(BASE_URL) 
+        fun createClient(): NetworkClient {
+            println("Membangun NetworkClient dengan BASE_URL: $BASE_URL")
+            return NetworkClient(BASE_URL)
+        }
     }
-
-    fun connect() {
-        println("Connecting to: $url")
-    }
+        fun connect() {
+            println("Connecting to: $url...")
+        }
 }
