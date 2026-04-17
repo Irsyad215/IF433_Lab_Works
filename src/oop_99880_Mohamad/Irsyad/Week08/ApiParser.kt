a3a05af251160700759cf1919f537f2056dc6d17
Cmardo215 <Mohammadirsyadrendyka@gmail.com>
2026-04-17 20:49:11 +0700

    week08: (task) implement robust ApiParser using Elvis and requireNotNull

---
+++ src/oop_99880_Mohamad/Irsyad/Week08/ApiParser.kt
@@ -0,0 +1,36 @@
+package oop_99880_Mohamad.Irsyad.Week08
+
+class ApiParser {
+
+    fun parseProduct(rawJason: Map<String, Any>): Product? {
+        val id = requireNotNull(rawJason["id"] as? String) {
+            "API Invalid: Missing ID"
+        }
+        val name = requireNotNull(rawJason["name"] as? String) {
+            "API Invalid: Missing Name"
+        }
+
+        val type = rawJason["type"] as? String
+
+        return when (type) {
+            "ELECTRONIC" -> {
+                val warranty = rawJason["warranty"] as? Int ?: 12
+                Electronic(id, name, warranty)
+            }
+            "CLOTHING" -> {
+                val size = rawJason["size"] as? String ?: "ALL Size"
+                Clothing(id, name, size)
+            }
+            else -> null
+        }
+    }
+
+    fun checkout(product: Product) {
+        val id = when (product) {
+            is Electronic -> product.id
+            is Clothing -> product.id
+        }
+        val transactionId = JavaPaymentService.ProccesPayment(id)!!
+        println("Payment Success!! Transaction id: $transactionId")
+    }
+}
