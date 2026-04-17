8e6361f82648a412916860ad9a5ed03fc08434ad
Cmardo215 <Mohammadirsyadrendyka@gmail.com>
2026-04-17 20:53:09 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_99880_Mohamad/Irsyad/Week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_99880_Mohamad.Irsyad.Week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String ProductId) {
+        return "TRX-" + ProductId + "-SUCCESS";
+    }
+}
