60827be274cb2f10336e6da87f3e6ce23d76450b
Cmardo215 <Mohammadirsyadrendyka@gmail.com>
2026-04-24 11:00:19 +0700

    week09: (task) print top performers using forEach

--- src/oop_99880_Mohamad/Irsyad/Week09/TradingDashboard.kt
+++ src/oop_99880_Mohamad/Irsyad/Week09/TradingDashboard.kt
@@ -36,4 +36,10 @@ fun main() {
         .map { it.pair }
         .toSet()
 
+    println("=== CRYPTO TRADING DASHBOARD ===")
+
+    println("\n--- TOP PERFORMERS (WIN) ---")
+    topPerformersString.forEach { println(it) }
+
+
 }
