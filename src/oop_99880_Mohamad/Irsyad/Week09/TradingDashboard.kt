e985cae1f0e40126c1d59fedd964f2307256bff2
Cmardo215 <Mohammadirsyadrendyka@gmail.com>
2026-04-24 11:00:44 +0700

    week09: (task) print worst performers and unique pairs

--- src/oop_99880_Mohamad/Irsyad/Week09/TradingDashboard.kt
+++ src/oop_99880_Mohamad/Irsyad/Week09/TradingDashboard.kt
@@ -41,5 +41,10 @@ fun main() {
     println("\n--- TOP PERFORMERS (WIN) ---")
     topPerformersString.forEach { println(it) }
 
+    println("\n--- WORST PERFORMERS (LOSS) ---")
+    worstPerformersString.forEach { println(it) }
+
+    println("\n--- UNIQUE TRADING PAIRS ---")
+    println(uniquePairs)
 
 }
