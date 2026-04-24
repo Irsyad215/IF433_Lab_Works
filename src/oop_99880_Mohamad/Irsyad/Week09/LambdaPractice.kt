ccf4c8e12db40b39c32172c8854cf8dbbc28425d
Cmardo215 <Mohammadirsyadrendyka@gmail.com>
2026-04-24 10:08:37 +0700

    week09: create basic lambdas using explicit and implicit syntax

---
+++ src/oop_99880_Mohamad/Irsyad/Week09/LambdaPractice.kt
@@ -0,0 +1,10 @@
+package oop_99880_Mohamad.Irsyad.Week09
+
+fun main() {
+    println("=== TEST LAMBDA ===")
+    val sumLambda = { a: Int , b: Int -> a + b }
+    println("hasil sum === ${sumLambda(5, 10)}")
+
+    val squareImplicit: (Int) -> Int = { it * it }
+    println("Hasil square: ${squareImplicit(4)}")
+}
