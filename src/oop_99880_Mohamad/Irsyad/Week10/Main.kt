0d4f6af00884c50d844f1dc1ea2d770285e5341e
Cmardo215 <Mohammadirsyadrendyka@gmail.com>
2026-05-07 19:38:42 +0700

    week10: test math operations and comparable constraints

--- src/oop_99880_Mohamad/Irsyad/Week10/Main.kt
+++ src/oop_99880_Mohamad/Irsyad/Week10/Main.kt
@@ -10,4 +10,9 @@ fun main() {
 
     println("\n=== TEST MULTIPLE PARAMETERES ===")
     val itemPrice = Pairbox("Bitcoin", 65000)
+
+    println("\n=== TEST CONSTRAINTS ===")
+    val math = MathBox(10.5, 20)
+    println("Total: ${math.sum()}")
+    println("Terbesar: ${getMax(45, 90)}")
 }
