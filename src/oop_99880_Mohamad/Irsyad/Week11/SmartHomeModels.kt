10b19639c6568240b5a6de867ba2248db252acc3
Cmardo215 <Mohammadirsyadrendyka@gmail.com>
2026-05-08 10:50:07 +0700

    week11: (task) create SmartDevice data class

---
+++ src/oop_99880_Mohamad/Irsyad/Week11/SmartHomeModels.kt
@@ -0,0 +1,8 @@
+package oop_99880_Mohamad.Irsyad.Week11
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false
+    var powerLoad: Int = 0
+)
