68bafe98c54b9de817434385ecaece155b241fc2
Cmardo215 <Mohammadirsyadrendyka@gmail.com>
2026-05-15 10:36:57 +0700

    week12: (task) implement hardware validation with throw

--- src/oop_99880_Mohamad/Irsyad/Week12/SmartFeeder.kt
+++ src/oop_99880_Mohamad/Irsyad/Week12/SmartFeeder.kt
@@ -1,2 +1,5 @@
 package oop_99880_Mohamad.Irsyad.Week12
 
+fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
+    require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
+}
