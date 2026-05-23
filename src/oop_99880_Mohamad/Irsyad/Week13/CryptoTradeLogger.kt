ea2109a324a14c02bfb5916dee5028a3ce8af540
Cmardo215 <Mohammadirsyadrendyka@gmail.com>
2026-05-23 14:48:32 +0700

    week13: (task) calculate total PnL from loaded trades

--- src/oop_99880_Mohamad/Irsyad/Week13/CryptoTradeLogger.kt
+++ src/oop_99880_Mohamad/Irsyad/Week13/CryptoTradeLogger.kt
@@ -58,4 +58,7 @@ fun main() {
 
     File("crypto_trades.csv").appendText("CORRUPT_ID,DOGEUSDT,Hold,XX,YY\n")
     println("Baris korup berhasil di-inject.")
+
+    val loadedData = loadTrades(path = "crypto_trades.csv")
+    val totalPnl = loadedData.sumOf { it.pnl }
 }
